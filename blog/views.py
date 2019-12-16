eb7f4a569cfe8583c6606f1bde0ab5d1c7b5e27a
qjltch <qjltch@foxmail.com>
2019-12-16 23:21:37 +0800

    编译器环境重新配置

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+import markdown
 # Create your views here.
 def index(requset):
     post_list=Post.objects.all().order_by('-create_time')
