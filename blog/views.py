0a27c63eb27d6ca584c4fa135fc936ea4b84c9c4
qjltch <qjltch@foxmail.com>
2019-12-16 23:39:00 +0800

    让博客支持Markdown

--- blog/views.py
+++ blog/views.py
@@ -12,4 +12,9 @@ def index(requset):
     })
 def detail(requset,pk):
     post=get_object_or_404(Post,pk=pk)
+    post.body=markdown.markdown(post.body,extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+    ])
     return render(requset,'blog/detail.html',context={'post':post})
